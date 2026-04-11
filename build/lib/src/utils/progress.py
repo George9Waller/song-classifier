af7fb0d64583af6284b7116272bf2a19d99af6f8
George Waller <54817483+George9Waller@users.noreply.github.com>
2026-04-11 16:37:44 +0100

    Remove build (#6)
    * Remove build
    
    * Add build to gitignore

--- build/lib/src/utils/progress.py
+++
@@ -1,9 +0,0 @@
-from rich.progress import SpinnerColumn, TextColumn, BarColumn, TaskProgressColumn
-
-PROGRESS_COLUMNS = [
-    SpinnerColumn(),
-    TextColumn("[progress.description]{task.description}"),
-    BarColumn(),
-    TaskProgressColumn(),
-    TextColumn("[cyan]{task.fields[filename]}"),
-]
